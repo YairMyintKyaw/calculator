94f0996b167b4d5e3df99c4922618157a396404e
Yzzzk <yemyintkyaw241220@gmail.com>
2022-02-28 00:39:49 +0630

    display UI of the calculator

--- script.js
+++ script.js
@@ -21,4 +21,46 @@ function operate(operator,...numbers){
         divide(numbers);
 }
 
-console.log(operate('*',2,2,2,3))
+
+/* Numbers and Operator buttons */
+
+const numberButtons = document.querySelectorAll('.buttonArea div')[0];
+const operators = document.querySelectorAll('.buttonArea div')[1];
+let text='9';
+let operatorSymbols=['AC','DEL','÷','x','-','+','=']
+let index=0;
+for(let i=0;i<4;i++){
+    //number buttons
+    const LnumberBtnRow=document.createElement('div');
+    LnumberBtnRow.classList.add('numberBtnRow');
+    for(let j=0;j<3;j++){
+
+        const numberBtn=document.createElement('div');
+        numberBtn.classList.add('numberBtn','leftNumber');
+        numberBtn.textContent=text=='0'?'x10^':text=='-1'?'.':text=='-2'?'0':text
+        LnumberBtnRow.append(numberBtn)
+        text--;
+    }
+    numberButtons.append(LnumberBtnRow)
+
+    //operators buttons
+    const RnumberBtnRow=document.createElement('div');
+    RnumberBtnRow.classList.add('numberBtnRow');
+    for(let j=0;j<2;j++){
+        if(index==7){
+            break;
+        }else{
+            const numberBtn=document.createElement('div');
+            numberBtn.classList.add('numberBtn','rightNumber');
+            if(index==0 || index==1){
+                numberBtn.classList.add('orangeBackground')
+            }
+            numberBtn.textContent=operatorSymbols[index];
+            RnumberBtnRow.append(numberBtn)
+            index++;
+        }
+        
+    }
+    operators.append(RnumberBtnRow)
+}
+
